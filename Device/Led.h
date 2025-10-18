7591eec37fe03b01e125a2f8a3e833430fb860be
John D. Haughton <anotherjohnh@gmail.com>
2025-10-18 15:35:31 +0100

    Initial

---
+++ Device/Led.h
@@ -0,0 +1,27 @@
+//-------------------------------------------------------------------------------
+// Copyright (c) 2024 John D. Haughton
+// SPDX-License-Identifier: MIT
+//-------------------------------------------------------------------------------
+
+// \brief Hardware LED
+
+#pragma once
+
+#if defined(HW_LED)
+#include "MTL/Pins.h"
+#include "MTL/Digital.h"
+#endif
+
+namespace hw {
+
+#if defined(HW_LED)
+
+using Led = MTL::Digital::Out<MTL::PIN_LED1>;
+
+#else
+
+using Led = bool;
+
+#endif
+
+} // namespace hw
